de2eeb7263dc19991863e8a8f2cb55070f58c49c
Adrien Joly <adrien@whyd.com>
2016-05-16 18:40:49 +0200

    improved ui: added link to spotify track + display search status

--- app/containers/App.js
+++ app/containers/App.js
@@ -1,30 +1,41 @@
 import React, { Component, PropTypes } from 'react';
 import style from './App.css';
 
+function searchTrack(name, callback) {
+  var url = 'https://api.spotify.com/v1/search?type=track&limit=1&q=' + encodeURIComponent(name);
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', url, true);
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState == 4) {
+      callback(null, JSON.parse(xhr.responseText).tracks.items[0]);
+    }
+  };
+  xhr.send();  
+}
+
 export default class App extends Component {
   constructor(props, context) {
     super(props, context);
-    this.state = { title: props.tab.title || '(no title)' };
-
-    // ask track to spotify api
-    var url = 'https://api.spotify.com/v1/search?type=track&limit=1&q=' + encodeURIComponent(props.tab.title.replace(' - YouTube', ''));
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
-    xhr.onreadystatechange = () => {
-      if (xhr.readyState == 4) {
-        var results = JSON.parse(xhr.responseText).tracks.items;
-        console.log('spotify results:', results);
-        this.setState({
-          title: results.length ? results[0].name : '(no result)'
-        });
-      }
-    }
-    xhr.send();
+    var title = props.tab.title.replace(' - YouTube', '');
+    this.state = {
+      status: 'Searching for ' + title + ' ...',
+      href: null,
+      title: null
+    };
+    // search for track on spotify => update UI
+    searchTrack(title, (err, res) => {
+      console.log('spotify result:', res);
+      this.setState({
+        status: res ? 'Found track:' : 'Sorry, not found... :-(',
+        href: res && res.external_urls.spotify,
+        title: res && res.name
+      });
+    });
   }
 
   render() {
     return (
-      <p>{this.state.title}</p>
+      <p>{this.state.status}&nbsp;<a target="_blank" href={this.state.href}>{this.state.title}</a></p>
     );
   }
 }
