3d6e6b0127dbde25d4e2d02abc731c8a15458df9
Adrien Joly <adrien@whyd.com>
2016-05-16 18:03:22 +0200

    when clicking on icon, searching and displaying track name from spotify, based on tab title

--- app/containers/App.js
+++ app/containers/App.js
@@ -2,9 +2,29 @@ import React, { Component, PropTypes } from 'react';
 import style from './App.css';
 
 export default class App extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = { title: props.tab.title || '(no title)' };
+
+    // ask track to spotify api
+    var url = 'https://api.spotify.com/v1/search?type=track&limit=1&q=' + encodeURIComponent(props.tab.title.replace(' - YouTube', ''));
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState == 4) {
+        var results = JSON.parse(xhr.responseText).tracks.items;
+        console.log('spotify results:', results);
+        this.setState({
+          title: results.length ? results[0].name : '(no result)'
+        });
+      }
+    }
+    xhr.send();
+  }
+
   render() {
     return (
-      <p>add to spotify?</p>
+      <p>{this.state.title}</p>
     );
   }
 }
--- chrome/extension/app.js
+++ chrome/extension/app.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom';
 import App from '../../app/containers/App';
 import './app.css';
 
-ReactDOM.render(
-  <App />,
-  document.querySelector('#root')
-);
+chrome.tabs.getSelected(null, function(tab) {
+  console.log('TAB', tab);
+  ReactDOM.render(
+    <App tab={tab} />,
+    document.querySelector('#root')
+  );
+});
